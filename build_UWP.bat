8d87fb96cdf3435b944efa6fcc95168e2151df85
arth <clemens@arth.co.at>
2018-12-11 21:33:37 +0100

    switch platform

--- build_UWP.bat
+++ build_UWP.bat
@@ -7,16 +7,22 @@ call "%VS140COMNTOOLS%..\..\..\Microsoft Visual Studio\2017\Community\VC\Auxilia
 
 cd %~dp0
 
-msbuild openSSL.sln /t:NT-Universal-10_0-Static-Unicode /p:Configuration=Release;Platform=x86
-if not %ERRORLEVEL% == 0 goto Finish
+IF "%bits%"=="x86" (
+  msbuild openSSL.sln /t:NT-Universal-10_0-Static-Unicode /p:Configuration=Release;Platform=x86
+  if not %ERRORLEVEL% == 0 goto Finish
+)
 
-msbuild openSSL.sln /t:NT-Universal-10_0-Static-Unicode /p:Configuration=Release;Platform=x64
-if not %ERRORLEVEL% == 0 goto Finish
+IF "%bits%"=="x64" (
+  msbuild openSSL.sln /t:NT-Universal-10_0-Static-Unicode /p:Configuration=Release;Platform=x64
+  if not %ERRORLEVEL% == 0 goto Finish
+)
 
-msbuild openSSL.sln /t:NT-Universal-10_0-Static-Unicode /p:Configuration=Release;Platform=ARM
-if not %ERRORLEVEL% == 0 goto Finish
+IF "%bits%"=="arm" (
+  msbuild openSSL.sln /t:NT-Universal-10_0-Static-Unicode /p:Configuration=Release;Platform=ARM
+  if not %ERRORLEVEL% == 0 goto Finish
+)
 
-echo #### Building Vizario done!
+echo #### Building OpenSSL done!
 
 :Finish
 popd
